247b93bda9033e9edfe98b6d63256b1f6215b402
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2021-12-17 17:15:52 +0000

    Add search option
    I had feedback that it is not clear how to select a city if more than one with the same name exist in the same country or in any country.
    I decided to add that functionality.
    I fetched data from the API endpoint for all the cities with the same name and then I will add a function to extract the weather for this particular city the user chooses.
    I have added the first part to the project and the cities are available to view.
    I added some JSX for that and added a onclick event with the function.
    I managed to get the API endpoint but will have to use geo coordinates as the json data does not contain ID for the city.

--- src/App.js
+++ src/App.js
@@ -13,18 +13,35 @@ const api = {
 function App() {
   const [query, setQuery] = useState('')
   const [weather, setWeather] = useState({})
+  const [city, setCity] = useState({})
+
+  const [lon, setLon] = useState({})
+  const [lat, setLat] = useState({})
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      // fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${api.key}`)
       .then(res => res.json())
       .then(result => {
-        setWeather(result)
+        setCity(result)
         setQuery('')
         document.getElementById('initialMessage').style.display = "none"
+        console.log(result.lon)
+        setLon(result.lan)
+        setLat(result.lon)
       })
     }
+  }
 
+  const searchWeather = (lon, lat) => {
+
+         fetch(`${api.base}weather?lat={lat}&lon={lon}&APPID=${api.key}`)
+            .then(res => res.json())
+            .then(result => {
+              setWeather(result)
+              console.log(result)
+            })
   }
 
 
@@ -65,9 +82,30 @@ function App() {
         <br></br>
         <p style={{fontSize: "12px"}}>Please note that the country code has to be added if the same city name is available in more than one country. For example, Cambridge, US or Cambridge, GB.</p>
         </div>
-        {(weather.message) ? (
-          <p className='error'>Please make sure the name is correct and try again</p>
-        ) :('')}
+
+        <div>
+          {(weather.message) ? (
+            <p className='error'>Please make sure the name is correct and try again</p>
+          ) :('')}
+        </div>
+
+
+        <div className='linksContainer'>
+          {(city.length > 0) ? (
+            <div>
+            {
+             console.log(city),
+              city.map((index, id) => {
+                return (
+                  <p className='linksCities' key={id} onClick={searchWeather}>{ index.name }, { index.state}, { index.country }
+                  </p>
+                )
+          })}
+          </div>
+          ) : ('')      
+          }
+        </div>
+
         {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
