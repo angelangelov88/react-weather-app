6e3388749609dfd6b8cbe4a4b67c45260f4ecbfe
Angel <angel.levski@gmail.com>
2021-12-21 15:46:47 +0000

    Added comments
    I added comments to make my script readable.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { useEffect, useState } from 'react'
-import env from 'react-dotenv';
+import { useState } from 'react'
 import { CountryCodes } from './CountryCodes'
 
 const API_KEY = process.env.REACT_APP_API_KEY
@@ -19,23 +18,25 @@ function App() {
   const [linksShown, setLinksShown] = useState(false)
   const [initialMessageShown, setInitialMessageShown] = useState(true)
 
+// This is the search function which is quering the API endpoint for the user input and then returns the result and sets the city state to it. It is used later in the rendering
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10&appid=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setCity(result)
+//Set the query to empty string to make sure the user can search another town
         setQuery('')
+//These states are used to make sure that the component that is needed is shown and vise-versa
         setInitialMessageShown(false)
         setLinksShown(true)
         setWeatherShown(false)
       })
-        }
+    }
   }
 
-
+//This is a function that gets its parameter from the user click event. Once the user clicks on the selected city, it gets its geo coordinates and they are used in the fetch request to get the weather for that location 
   const searchWeather = (index) => {
-
          fetch(`${api.base}weather?lat=${index.lat}&lon=${index.lon}&units=metric&APPID=${api.key}`)
             .then(res => res.json())
             .then(result => {
@@ -43,9 +44,9 @@ function App() {
               setLinksShown(false)
               setWeatherShown(true)
             })
-  }
+          }
 
-
+//This is a date builder function that gets the current day
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -58,15 +59,17 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
-
+//The DOM's elements are being created here
   return (
     <div className={
+//This part evaluates the weather and based on that displays an image (cold for less than 5, autumn image between 6 and 16 and beach for over 16)
       (typeof weather.main != "undefined") 
       ? (
         (weather.main.temp > 16) ? 'app warm' :
         (weather.main.temp < 5) ? 'app' : 'app cold' ) 
       : 'app cold'}>
       <main>
+{/* This is the search bar */}
         <div className="search-box">
           <input 
             type="text"
@@ -77,21 +80,21 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        
+{/* This is the initial message shown which state changes when the search function is called and it gets hidden */}
         { initialMessageShown && 
           <div id='initialMessage'>
             <p>Please type a city or country name to get the current weather</p>
-        </div>
+          </div>
         }
-
+{/* This is the error message which is displayed if the search function returns an empty array */}
         <div>
-          {(city.length == 0) ? (
+          {(city.length === 0) ? (
             <p className='error'>Please make sure the name is correct and try again</p>
-          ) :('')
+            ) :('')
         }
         </div>
 
-
+{/* This is the cities list which are returned from the search function. I used map method to show each element on a separate line */}
     { linksShown &&      
        <div className='linksContainer'>
           {(city.length > 0) ? (
@@ -99,8 +102,10 @@ function App() {
             {
               city.map((index, id) => {
                 return (
-                  <p className='linksCities' key={id} onClick={() => searchWeather(index)}>{ index.name + ','} { 
-                    typeof(index.state) != 'undefined'  && index.state + ','  
+                  <p className='linksCities' key={id} onClick={() => searchWeather(index)}>
+                    { index.name + ',' } { 
+                    typeof(index.state) != 'undefined' && index.state + ',' 
+// I used the filter method here to loop through the countyCode json file and find the country name for this particular country code. Then I display the whole name of the country in the cities list 
                   } { index.country ? `${CountryCodes.filter(country => country.alpha2 === index.country.toLowerCase())[0].name}` : ""}
                   </p>
                 )
@@ -110,12 +115,19 @@ function App() {
           }
         </div>
 }
-
+{/* This is where the weather is shown. I show the city name, full country name, date, temperature, feels like temperature and the weather condition. There is a condition to show this element only if there is any data in it in order to ensure that it does not show when the user tries to search for another town */}
         { weatherShown  ? (
           <div id="weather-container" style={{ display: weatherShown ? "block" : "none" }}>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
+              <div className="location">{weather.name}
+              <p style={{fontSize: "20px"}}>
+              {
+              weather.sys.country ? `${CountryCodes.filter(country => country.alpha2 === weather.sys.country.toLowerCase())[0].name}` : ""}
+              </p>
+              </div>
+              <br></br>
+              <div className="date">{dateBuilder(new Date())}
+              </div>
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°C
