e34c2e7ce18ad6a571a72f7a84a4734cd085bf31
Angel <82048582+angelangelov88@users.noreply.github.com>
2026-08-16 17:18:38 +0100

    Updates for mobile
    Made keyboard hide when user clicks Enter in search
    Some updates to make the bg image not jump on scroll on mobile

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,33 @@
+import { useRef } from 'react'
+
 type Props = {
   query: string
   onChange: (value: string) => void
   onKeyDown: (evt: React.KeyboardEvent<HTMLInputElement>) => void
 }
 
-const SearchBar = ({ query, onChange, onKeyDown }: Props) => (
-  <div className="search-box">
-    <input
-      type="text"
-      className="search-bar"
-      aria-label="Search for a city"
-      placeholder="Search..."
-      value={query}
-      onChange={(e) => onChange(e.target.value)}
-      onKeyDown={onKeyDown}
-    />
-  </div>
-)
+const SearchBar = ({ query, onChange, onKeyDown }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    if (evt.key === 'Enter') inputRef.current?.blur()
+    onKeyDown(evt)
+  }
+
+  return (
+    <div className="search-box">
+      <input
+        ref={inputRef}
+        type="text"
+        className="search-bar"
+        aria-label="Search for a city"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => onChange(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  )
+}
 
 export default SearchBar
